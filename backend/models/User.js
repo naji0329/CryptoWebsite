5499c5f804a5913f499f0b80ba8fb567a288c23b
NAJI2329 <be1512t6@gmail.com>
2022-03-11 05:13:23 +0900

    initial commit

---
+++ backend/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  phonenumber: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('user', UserSchema);
